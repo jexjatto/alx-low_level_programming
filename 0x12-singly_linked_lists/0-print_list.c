a5bfa8af6db8eb55d6e67db9739b076b702f7d0f
jexjatto <jextrim@gmail.com>
2022-10-21 22:29:15 +0100

    new codes for 0 print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,14 @@
 #include "lists.h"
-
 /**
  * print_list - prints list.
  * @h: param list
  * Return: struct
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t l_node;
+	size_t nodelen;
 
-	l_node = 0;
+	nodelen = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -18,7 +16,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		l_node++;
+		nodelen++;
 	}
-	return (l_node);
+	return (nodelen);
 }
