aee814b9d3d85c384ae7c614021df4825ad7d9a4
jexjatto <jextrim@gmail.com>
2022-10-25 19:27:13 +0100

    new code for 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,43 +1,52 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a node at aan index of list
- * @head: beginning of trhe list
- * @idx: index of list
- * @n: data value
- * Return: address of list
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newnode, *temp;
-	unsigned int i = 0;
+	unsigned int i;
+	listint_t *new;
+	listint_t *h;
 
-	temp = *head;
-	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL)
+	h = *head;
+
+	if (idx != 0)
 	{
-		return (NULL);
+		for (i = 0; i < idx - 1 && h != NULL; i++)
+		{
+			h = h->next;
+		}
 	}
-	newnode->n = n;
+
+	if (h == NULL && idx != 0)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
 	if (idx == 0)
 	{
-		newnode->next = temp;
-		*head = newnode;
-		return (*head);
-		free(newnode);
+		new->next = *head;
+		*head = new;
 	}
-	while (i < idx)
+	else
 	{
-		temp = temp->next;
-		if (temp == NULL)
-		{
-			free(newnode);
-			return (NULL);
-		}
-		i++;
+		new->next = h->next;
+		h->next = new;
 	}
-	newnode->next = temp->next;
-	temp->next = newnode;
-	return (newnode);
-	free(newnode);
+
+	return (new);
 }
